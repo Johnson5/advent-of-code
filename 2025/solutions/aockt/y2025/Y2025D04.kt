06c71725ef36493378f301b7d70400e44dc708fa
Jan Lobee <jan.lobee@nslogin.nl>
2025-12-04 19:40:56 +0100

    day 04

--- 2025/solutions/aockt/y2025/Y2025D04.kt
+++ 2025/solutions/aockt/y2025/Y2025D04.kt
@@ -1,19 +1,73 @@
 package aockt.y2025
 
 import io.github.jadarma.aockt.core.Solution
+import utils.Coordinate
 import utils.Grid
-import kotlin.collections.component1
-import kotlin.collections.component2
 
 object Y2025D04 : Solution {
 
     private fun parseInput(input: String): Grid<Char> =
         Grid(input.lines()) { it }
 
-    override fun partOne(input: String): Long {
-        return 3L
+    override fun partOne(input: String): Int {
+        val grid = parseInput(input)
+        val o = Coordinate(0, 0)
+        val surrounding = listOf(
+            o.up(),
+            o.down(),
+            o.left(),
+            o.right(),
+            o.left() + o.up(),
+            o.right() + o.up(),
+            o.left() + o.down(),
+            o.right() + o.down()
+        )
+        return grid.indices().count { c ->
+            if (grid.at(c) == '.') return@count false
+            var count = 0
+            for(s in surrounding) {
+                if(grid.at(c+s) != null && grid.at(c + s) == '@') {
+                    count ++
+                }
+            }
+            return@count count < 4
+        }
     }
 
-    override fun partTwo(input: String): Long =
-        TODO()
+    override fun partTwo(input: String): Int {
+        val grid = parseInput(input)
+        val o = Coordinate(0, 0)
+        val surrounding = listOf(
+            o.up(),
+            o.down(),
+            o.left(),
+            o.right(),
+            o.left() + o.up(),
+            o.right() + o.up(),
+            o.left() + o.down(),
+            o.right() + o.down()
+        )
+        var iterate = true
+        var total = 0
+        while(iterate) {
+            val reachable = grid.indices().count { c ->
+                if (grid.at(c) == '.') return@count false
+                var count = 0
+                for(s in surrounding) {
+                    if(grid.at(c+s) != null && grid.at(c + s) == '@') {
+                        count ++
+                    }
+                }
+                if(count < 4){
+                    grid.set(c, '.')
+                    total++
+                    return@count true
+                } else {
+                    return@count false
+                }
+            }
+            if(reachable == 0) iterate = false
+        }
+        return total
+    }
 }
